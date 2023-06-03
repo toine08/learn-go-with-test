e1245f4ee0ebc17fb4babf7f333879ad284aab22
toto <riedotoine@gmail.com>
2023-06-03 16:57:20 +0200

    finished hello world starting chapter 2

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -2,10 +2,35 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	got := Hello("Toine")
-	want := "Hello, Toine"
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Toine", "")
+		want := "Hello, Toine"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, world' when an empty string is supplied",
+		func(t *testing.T) {
+			got := Hello("", "")
+			want := "Hello, World"
+
+			assertCorrectMessage(t, got, want)
+		})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Loic", "French")
+		want := "Salut, Loic"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -2,31 +2,32 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanish = "Spanish"
-const spanishHelloPrefix = "Hola, "
-const french = "French"
-const frenchHelloPrefix = "Salut, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Salut, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishHelloPrefix
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "french":
 		prefix = frenchHelloPrefix
 	case "spanish":
 		prefix = spanishHelloPrefix
+
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return prefix + name
+	return
 }
 
 func main() {
